ad181460de2e3aa06795ba6bcbd62a38a170ac91
mac <mac@Tariq-Mac-Book.local>
2026-01-19 18:01:11 +0500

    ios files

--- ios/forntier_pay_mobile_app/AppDelegate.swift
+++ ios/forntier_pay_mobile_app/AppDelegate.swift
@@ -2,11 +2,11 @@ import UIKit
 import React
 import React_RCTAppDelegate
 import ReactAppDependencyProvider
+import Firebase
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
   var window: UIWindow?
-
   var reactNativeDelegate: ReactNativeDelegate?
   var reactNativeFactory: RCTReactNativeFactory?
 
@@ -14,6 +14,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil
   ) -> Bool {
+
+    FirebaseApp.configure()
+
     let delegate = ReactNativeDelegate()
     let factory = RCTReactNativeFactory(delegate: delegate)
     delegate.dependencyProvider = RCTAppDependencyProvider()
@@ -31,8 +34,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     return true
   }
-}
-
+  }
+  
 class ReactNativeDelegate: RCTDefaultReactNativeFactoryDelegate {
   override func sourceURL(for bridge: RCTBridge) -> URL? {
     self.bundleURL()
